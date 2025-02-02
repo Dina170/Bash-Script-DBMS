7550898dcf51a69f2f858b99432faed391a85a00
dina <dinaelhadidy966@gmail.com>
2025-02-02 21:52:47 +0200

    Feature: Create table

---
+++ tableMenu.sh
@@ -0,0 +1,53 @@
+createtb() {
+    read -p "Enter table name: " name
+    if [ -z $name ] 
+    then
+        echo please enter table name
+    elif [ -f db/$CURRENT_DB/$name ]
+    then
+        echo table already exists
+    elif [[ ! $name =~ ^[a-zA-Z0-9]+$ ]]
+    then
+        echo name can not contain special characters
+    else
+        columns=()
+        types=()
+        
+        read -p "enter primary key name: " pk
+        read -p "enter primary key type[int/string]: " pktype
+
+        columns+=($pk)
+        types+=($pktype)
+        while true
+        do
+            read -p "enter field name or done to exit: " field
+            if [ $field == "done" ]
+            then
+                echo exiting table
+                break
+            fi
+            read -p "enter field type[int/string]: " ftype
+
+            columns+=($field)
+            types+=($ftype)
+
+        done
+        touch db/$namedb/$CURRENT_DB/$name db/$CURRENT_DB/$name.meta
+        echo "Columns: ${columns[*]}" > "db/$CURRENT_DB/$name.meta"
+        echo "Types: ${types[*]}" >> "db/$CURRENT_DB/$name.meta"
+        echo "PrimaryKey: $pk" >> "db/$CURRENT_DB/$name.meta"
+        echo table created successfully
+    fi
+}
+
+select choice in "create table" "list tables" "drop table" "exit"
+do
+        case $REPLY in
+                1) createtb;;
+                2) ls db;;
+                3) connectTodb;;
+                4) dropdb;;
+                5) exit;;
+                *) echo not a valid option
+        esac
+done
