eb3d2a52161feabb8cf4cbf2aedcd26a918b9f7a
dina <dinaelhadidy966@gmail.com>
2025-02-10 06:06:54 -0500

    Handle deleting the last row

--- table/delete.sh
+++ table/delete.sh
@@ -3,11 +3,18 @@ read -p "Enter Table Name: " name
 if [[ ! -f "db/$CURRENT_DB/$name" ]]; 
 then
     echo "Table does not exist!"
-    exit 1
+    return
 fi
 
 read -p "Enter Primary Key value to delete: " key
-grep -v "^$key|" "db/$CURRENT_DB/$name" >"db/$CURRENT_DB/temp" && mv "db/$CURRENT_DB/temp" "db/$CURRENT_DB/$name"
-echo " Row deleted successfully"
+grep -v "^$key|" "db/$CURRENT_DB/$name" > "db/$CURRENT_DB/temp"
 
+if [[ ! -s "db/$CURRENT_DB/temp" ]]
+then
+    rm "db/$CURRENT_DB/$name"
+    echo "Table is now empty and deleted"
+else
+    mv "db/$CURRENT_DB/temp" "db/$CURRENT_DB/$name"
+    echo "row deleted successfully"
+fi
 
